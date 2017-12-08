8475eec14306342c38bca45aa2d889d069788141
Gage Thomas <gageathomas@gmail.com>
2017-12-07 21:28:23 -0800

    api call 2 working with new model

---
+++ Models/ExerciseItemModelTwo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+//using TapFitness.Models;
+
+using Newtonsoft.Json;
+namespace TapFitness.Models
+{
+    public static class ExerciseItemModelTwo
+    {
+        public partial class ExerciseTwo
+        {
+            [JsonProperty("count")]
+            public long Count { get; set; }
+
+            [JsonProperty("next")]
+            public string Next { get; set; }
+
+            [JsonProperty("previous")]
+            public object Previous { get; set; }
+
+           // [JsonProperty("results")]
+           // public Result[] Results { get; set; }
+        }
+
+        public partial class Result
+        {
+          //  [JsonProperty("category")]
+          //  public Category Category { get; set; }
+
+            [JsonProperty("description")]
+            public string Description { get; set; }
+
+           // [JsonProperty("equipment")]
+           // public Category[] Equipment { get; set; }
+
+           // [JsonProperty("muscles")]
+           // public Muscle[] Muscles { get; set; }
+
+            [JsonProperty("muscles_secondary")]
+            public Muscle[] MusclesSecondary { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+        public partial class Muscle
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("is_front")]
+            public bool IsFront { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+        public partial class Category
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+        public partial class ExerciseTwo
+        {
+            
+            public static ExerciseTwo FromJson(string json) => JsonConvert.DeserializeObject<ExerciseTwo>(json, Converter.Settings);
+        }
+
+        //public static class Serialize
+        //{
+           
+        public static string ToJson(this ExerciseTwo self) => JsonConvert.SerializeObject(self, Converter.Settings);
+		
+        //}
+
+        public class Converter
+        {
+            public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                DateParseHandling = DateParseHandling.None,
+            };
+        }
+
+    }
+}
+
--- ViewModels/PlanPageViewModel.cs
+++ ViewModels/PlanPageViewModel.cs
@@ -4,15 +4,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Prism.Navigation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.ObjectModel;
+using static TapFitness.Models.ExerciseItemModelTwo;
 
 namespace TapFitness.ViewModels
 {
     public class PlanPageViewModel : BindableBase, INavigationAware
     {
+        public DelegateCommand PlanAPICommand { get; set; }
+
+		private ObservableCollection<ExerciseTwo> _exerciseCollectionTwo = new ObservableCollection<ExerciseTwo>();
+		public ObservableCollection<ExerciseTwo> ExerciseCollectionTwo
+		{
+			get { return _exerciseCollectionTwo; }
+			set { SetProperty(ref _exerciseCollectionTwo, value); }
+		}
+
         public PlanPageViewModel()
         {
-            INavigationService _navigationService;
+            //INavigationService _navigationService;
             //goes to nothing currently
+            PlanAPICommand = new DelegateCommand(SelectPlanFunc);
+        }
+
+        internal async void SelectPlanFunc()
+        {
+			HttpClient client = new HttpClient();
+
+			client.DefaultRequestHeaders.Authorization =
+					  new AuthenticationHeaderValue("Token", "a4d10b090f48c641a9590b6c34dcb4e87eb6e4cd");
+
+			var uri = new Uri(
+			 string.Format(
+					$"https://wger.de/api/v2/exercise/"));
+
+			var response = await client.GetAsync(uri);
+			ExerciseTwo exerciseDataTwo = null;
+
+			if (response.IsSuccessStatusCode)
+			{
+				var contentTwo= await response.Content.ReadAsStringAsync();
+				exerciseDataTwo = ExerciseTwo.FromJson(contentTwo);
+
+			}
+			ExerciseCollectionTwo.Add(exerciseDataTwo);
+			//AddToWeightResults();
         }
 
 		public void OnNavigatedFrom(NavigationParameters parameters)
