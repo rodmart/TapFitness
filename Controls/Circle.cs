40654505182f99487638dbe2579579921ef53cfc
rodmart <marti630@cougars.csusm.edu>
2017-12-15 10:23:01 -0800

    Added externa resource:SkiaSharp

---
+++ Controls/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace TapFitness.Controls
+{
+    public partial class Circle: ContentView
+    {
+        public Circle()
+        {
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            var info = args.Info;
+            var surface = args.Surface;
+            var canvas = surface.Canvas;
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+        }
+    }
+}
